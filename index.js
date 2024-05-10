6eda07745a3fa000022dd0b52f5955860cc42e75
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-05-10 13:47:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ let tiles = document.getElementsByClassName('tile')
 
 console.log(tiles)
 for(i=0;i<tiles.length;i++){
+    // game loop
     tiles[i].addEventListener('click',(e)=>
-        e.target.innerHTML="<h1>X<h1>"
+       {
+     e.target.innerHTML="<h1>X<h1>"
+        console.log(e)
+     e.target.previousElementSibling.innerHTML = "<h1>O<h1>"
+       }
     )
 }
 document.getElementById("clear").addEventListener('click',()=>{
