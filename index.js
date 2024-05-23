f64e515a7c748f5955d9b79fe8a5db216a5e838a
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-05-23 08:12:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ let tiles = document.getElementsByClassName('tile')
 
 console.log(tiles)
 for(i=0;i<tiles.length;i++){
-    tiles[i].innerHTML = ""
+  console.log(tiles[i].id)
+    tiles[i].innerHTML = tiles[i].id
     tiles[i].addEventListener('click',(e)=>
        {
         if(e.target.innerHTML == ""){
           e.target.innerHTML="<h1>X<h1>"
-          console.log(e)
+          let classN =e.target.className; 
+          e.target.className =classN+" X"; 
+          console.log(classN)
+          let Xtiles = document.getElementsByClassName('X');
+          for(let j=0;j<Xtiles.length;j++){
+            //if(Xtiles[j].id.includes('a'))
+          }
           if(e.target.previousElementSibling.innerHTML == ""){
             e.target.previousElementSibling.innerHTML = "<h1>O<h1>"
           }
