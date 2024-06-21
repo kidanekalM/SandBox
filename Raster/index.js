8fe90cab9ecdf39eb4a14dc9b0a1cc2d41c3cbb7
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-06-21 08:55:17 +0300

    added event for btn

--- Raster/index.js
+++ Raster/index.js
@@ -1,12 +1,23 @@
 let numRow = 30
 let numCol = 30
+let pixels = new Array(30)
 for(i=0;i<numRow;i++){
     let row = document.createElement('div');
     row.className = "row"
+    pixels[i] = new Array(30)
     for(j=0;j<numCol;j++){
         let pixel = document.createElement('div')
         pixel.className = "pixel"
         row.appendChild(pixel)
+        pixels[i][j] = pixel
     }
     document.getElementById("wrapper").appendChild(row)
 }
+// we reverse the array to resemble the cartesian plane
+pixels = pixels.reverse()
+
+pixels[7][9].className += ' set'
+
+document.getElementById("run").addEventListener('click',(e)=>{
+    console.log(document.getElementById("txtCode").value)
+})
