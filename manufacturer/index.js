83e9402f0645a493099b31021e6add320b92bdea
kidanekal <akidanekal@gmail.com>
2025-11-22 19:06:10 +0300

    UPD add pill

--- manufacturer/index.js
+++ manufacturer/index.js
@@ -1,7 +1,27 @@
 var capconts = Array.from(document.getElementsByClassName("capcont"));
+var circleconts = Array.from(document.getElementsByClassName("circlecont"));
+
+circleconts.map((circlecont,indx)=>{
+    circlecont.style=`animation-name: none;`
+})
+setTimeout(()=>{
+    circleconts.map((circlecont,indx)=>{
+        circlecont.style=`animation-name: rotate;`
+    })
+    capconts.map((capcont,indx)=>{
+    capcont.style=`transform: rotate(${(((increments)/(capconts.length)))*(indx)}deg);`
+    // capcont.style=`animation-name: rotate;`
+        })
+},3000)
 console.log("capconts",capconts)
 var increments = 360
 
 capconts.map((capcont,indx)=>{
-capcont.style=`transform: rotate(${(((increments)/(capconts.length)))*(indx)}deg);`
+    const moveInDelay = indx * 150;
+    capcont.style.animationName = "movein";
+    capcont.style.animationDuration = "700ms";
+    capcont.style.animationTimingFunction = "ease-out";
+    capcont.style.animationIterationCount = "1";
+    capcont.style.animationFillMode = "forwards";
+    capcont.style.animationDelay = `${moveInDelay}ms`;
 })
